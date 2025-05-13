7a2936de9eef139181d557ebdc9371da5c5fabda
JesusG04 <126627917+JesusG04@users.noreply.github.com>
2025-05-12 21:35:13 -0600

    Solucion de eventos

---
+++ actions/events.ts
@@ -0,0 +1,25 @@
+"use server"
+import { dataRef, child, get } from "@/src/firebase/firebaseConfig"
+
+import { EventMapSchema } from "@/src/schemas";
+
+export async function getEvents()  {
+
+    const eventoRef = child(dataRef, "Evento"); 
+
+    try {
+        const eventSnapShot = await get(eventoRef);
+        if (eventSnapShot.exists()) {
+            const data = eventSnapShot.val();
+                  // Parsear los datos usando el esquema de eventos
+            const events = EventMapSchema.parse(data);
+
+            return events;
+        }
+        return null; // 🔄 antes era `true`
+    } catch (error) {
+        console.error("Error al obtener eventos:", error);
+        return null; // 🔄 antes era `true`
+    }
+
+}
--- components/pages/home/events/EventsWrapper.tsx
+++ components/pages/home/events/EventsWrapper.tsx
@@ -1,29 +1,17 @@
 import Events from "./Events";
-import { EventMapSchema } from "@/src/schemas";
-import { dataRef, child, get } from "@/src/firebase/firebaseConfig";
+
+import { getEvents } from "@/actions/events";
 
 const EventsWrapper = async () => {
 
-  const eventoRef = child(dataRef, "Evento");
-
-  try {
-    const snapshot = await get(eventoRef);
-    if (snapshot.exists()) {
-      const data = snapshot.val();
-      // Parsear los datos usando el esquema de eventos
-      const events = EventMapSchema.parse(data);
-      // Pasar los eventos al componente `Events`
-      return <Events data={events} />;
-    }
-    console.log('error en la consulta');
-  } catch (error) {
-    console.log('error');
-    
-    console.error("Error al obtener eventos:", error);
-    // Puedes mostrar un mensaje de error o retornar algo por defecto si la API falla
-    return <div>Error al cargar los eventos. Intenta nuevamente más tarde.</div>;
-  }
-
+  const events = await getEvents();
+  console.log(events);
+  
+  return events ? (
+    <Events data={events} />
+  ) : (
+    <div>No se pudieron cargar los eventos.</div>
+  );
 };
 
 export default EventsWrapper;
